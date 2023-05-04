d0a6d20b8edd28ab506e6aa0e450411072528e17
leelethal816 <zhangtianyi816@gmail.com>
2023-05-04 15:12:21 -0500

    Initial Commit

--- FeedApp/admin.py
+++ FeedApp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Relationship, Post, Comment, Like
 
-
+admin.site.register(Profile)
+admin.site.register(Relationship)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
